0c0b90d97e6ad55bf98f828d59d69a0266deffb3
Benjamin Perseghetti <perseghetti@gmail.com>
2019-02-13 17:07:32 -0500

    name change for tag detections

--- apriltag3_ros/src/continuous_detector.cpp
+++ apriltag3_ros/src/continuous_detector.cpp
@@ -56,7 +56,7 @@ void ContinuousDetector::onInit ()
       it_->subscribeCamera("image_rect", 1,
                           &ContinuousDetector::imageCallback, this);
   tag_detections_publisher_ =
-      nh.advertise<AprilTagDetectionArray>("tag_detections", 1);
+      nh.advertise<AprilTagDetectionArray>("AprilTagDetectionArray", 1);
   if (draw_tag_detections_image_)
   {
     tag_detections_image_publisher_ = it_->advertise("tag_detections_image", 1);
